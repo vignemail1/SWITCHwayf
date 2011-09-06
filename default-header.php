0043d0774ff604ce677aafd27a9e30a1515c0195
haemmer <haemmer@630430ee-30e2-4e44-a323-da843f692fab>
2011-09-06 10:56:48 +0000

    Corrected body onLoad issue so that focus now also works

--- default-header.php
+++ default-header.php
@@ -1,64 +1,113 @@
 <?php // Copyright (c) 2011, SWITCH - Serving Swiss Universities ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html><head>
+<html>
+<head>
 	<title><?php echo getLocalString('title') ?></title> 
 	<meta http-equiv="content-type" content="text/html; charset=utf-8">
 	<meta name="keywords" content="Discovery Service, WAYF, Shibboleth">
 	<meta name="description" content="Choose your home organization to authenticate">
-	<style type="text/css"><!--
-	<? printCSS() ?>
--->
-</style>
-</head>
-
-<body onLoad="preventIframeEmbedding(); setFocus();">
-<script language="JavaScript" type="text/javascript">
-<!--
-
-function preventIframeEmbedding(){
-	if (top != self) {
-		top.location = self.location;
-	}
+	<script language="JavaScript" type="text/javascript">
+	<!--
 	
-}
-
-function setFocus(){
-	// Skip this if we cannot access the form elements
-	if (!document.IdPList || !document.IdPList.Select){
-		return;
-	}
-	
-	// Set focus to submit button unless autofocus is supported
-	if (!("autofocus" in document.createElement("input"))) {
-		document.IdPList.Select.focus();
-	}
-}
-
-function showConfirmation(){
-	
-	return confirm(unescape('<?php echo getLocalString('confirm_permanent_selection', 'js') ?>'));
-}
-
-function showPermanentConfirmation(){
-	
-	return alert(unescape('<?php echo getLocalString('permanent_cookie_note', 'js') ?>'));
-}
-
-function checkForm(){
-	if(document.IdPList.user_idp && document.IdPList.user_idp.selectedIndex == 0){
-		alert(unescape('<?php echo getLocalString('make_selection', 'js') ?>'));
-		return false;
-	} else {
-		if (document.IdPList.permanent && document.IdPList.permanent.checked){
-			return showConfirmation();
-		} else {
-			return true;
+	// Prevent that WAYF is loaded in an IFRAME
+	function preventIframeEmbedding(){
+		if (top != self) {
+			top.location = self.location;
 		}
 	}
-}
+	
+	// Set focus to submit button or drop down list
+	function setFocus(){
+		// Skip this if we cannot access the form elements
+		if (
+			!document.IdPList || 
+			!document.IdPList.Select
+			){
+			return;
+		}
+		
+		if (
+				document.IdPList.user_idp && 
+				document.IdPList.user_idp.selectedIndex == 0
+			){
+			// Set focus to select
+			document.IdPList.user_idp.focus();
+		} else {
+			// Set focus to submit button
+			document.IdPList.Select.focus();
+		}
+	}
+	
+	// Confirm action
+	function showConfirmation(){
+		
+		return confirm(unescape('<?php echo getLocalString('confirm_permanent_selection', 'js') ?>'));
+	}
+	
+	// Confirm permanent selection
+	function showPermanentConfirmation(){
+		
+		return alert(unescape('<?php echo getLocalString('permanent_cookie_note', 'js') ?>'));
+	}
+	
+	// Perform input validation on WAYF form
+	function checkForm(){
+		if(
+			document.IdPList.user_idp && 
+			document.IdPList.user_idp.selectedIndex == 0
+		){
+			alert(unescape('<?php echo getLocalString('make_selection', 'js') ?>'));
+			return false;
+		} else {
+			if (document.IdPList.permanent && document.IdPList.permanent.checked){
+				return showConfirmation();
+			} else {
+				return true;
+			}
+		}
+	}
+	
+	//Setup the dom ready event
+	function DomReady(func){
+		if(document.addEventListener){
+			//W3C
+			document.addEventListener("DOMContentLoaded", func, false);
+		} else {
+			//IE
+			document.onreadystatechange = function(){readyState(func)}
+		}
+	}
+	
+	//IE ready execution function
+	function readyState(func){
+		//dom is ready for interaction
+		if(document.readyState == "interactive"){
+			func();
+		}
+	}
+	
+	// Init WAYF
+	function init(){
+		preventIframeEmbedding();
+		setFocus();
+	}
+	
+	// Add new DomReady function
+	window.onDomReady = DomReady;
+	
+	// Attach init function
+	window.onDomReady(init);
+	
+	-->
+	</script>
+	<style type="text/css">
+	<!--
+	<? printCSS() ?>
+	-->
+	</style>
+</head>
 
--->
-</script>
+<body>
 
 <div id="container">
 	<div class="box">
