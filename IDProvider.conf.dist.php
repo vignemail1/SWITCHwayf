8995dbcd164f2282c8e60fc3facc7e97429268b4
haemmer <haemmer@630430ee-30e2-4e44-a323-da843f692fab>
2014-12-05 13:27:38 +0000

    Removed all SWITCHaai specific examples

--- IDProvider.conf.dist.php
+++ IDProvider.conf.dist.php
@@ -14,71 +14,71 @@
 
 // Category
 $IDProviders['university'] = array (
-		'Type' => 'category',
-		'Name' => 'Universities',
+        'Type' => 'category',
+        'Name' => 'Universities',
 );
 
 
-$IDProviders['https://idp.epfl.ch/idp/shibboleth'] = array(
-        'SSO' => 'https://idp.epfl.ch/idp/profile/Shibboleth/SSO',
-        'Name' => 'EPF Lausanne',
+$IDProviders['https://test-uni1.example.org/idp/shibboleth'] = array(
+        'SSO' => 'https://test-uni1.example.org/idp/profile/Shibboleth/SSO',
+        'Name' => 'Test University 1',
         'Type' => 'university',
 );
 
-$IDProviders['https://aai-logon.ethz.ch/idp/shibboleth'] = array(
-        'SSO' => 'https://aai-logon.ethz.ch/idp/profile/Shibboleth/SSO',
-        'Name' => 'ETH Z&uuml;rich',
-        'de' => array ('Name' => 'ETH Z&uuml;rich'),
-        'en' => array ('Name' => 'ETH Zurich'),
+$IDProviders['https://test-uni2.example.org/idp/shibboleth'] = array(
+        'SSO' => 'https://test-uni1.example.org/idp/profile/Shibboleth/SSO',
+        'Name' => 'Test Universit&auml;t 2',
+        'de' => array ('Name' => 'Test Universit&auml;t 2'),
+        'en' => array ('Name' => 'Test University 2'),
         'Type' => 'university',
-	'IP' => array ('193.166.2.0/24','129.132.0.0/16'),
+        'IP' => array ('193.166.2.0/24','129.132.0.0/16'),
 );
 
-$IDProviders['https://aai-idp.uzh.ch/idp/shibboleth'] = array(
-        'SSO' => 'https://aai-idp.uzh.ch/idp/profile/Shibboleth/SSO',
-        'Name' => 'Universit&auml;t Z&uuml;rich',
+$IDProviders['https://test-uni3.example.org/idp/shibboleth'] = array(
+        'SSO' => 'https://test-uni3.example.org/idp/profile/Shibboleth/SSO',
+        'Name' => 'Test University 3',
         'Type' => 'university',
-	'Realm' => 'uzh.ch',
+        'Realm' => 'example.org',
         'en' => array ('Keywords' => 'Zurich Irchel+Park'),
 );
 
 
 // Category
 $IDProviders['vho'] = array (
-		'Type' => 'category',
-		'Name' => 'Virtual Home Organizations',
+        'Type' => 'category',
+        'Name' => 'Virtual Home Organizations',
 );
 
 // An example of a configuration with multiple network blocks and multiple languages 
-$IDProviders['https://aai-logon.vho-switchaai.ch/idp/shibboleth'] = array (
-		'Type' => 'vho',
-		'Name' => 'Virtual Home Organisation',
-		'en' => array (
-			'Name' => 'Virtual Home Organisation',
-			'Keywords','Zurich Switzerland',
-			),
-		'de' => array (
-			'Name' => 'Virtuelle Home Organisation',
-			'Keywords','Zürich Schweiz',
-			),
-		'fr' => array ('Name' => 'Home Organisation Virtuelle'),
-		'it' => array ('Name' => 'Virtuale Home Organisation'),
-		'IP' => array ('130.59.6.0/16','127.0.0.0/24'),
-		'SSO' => 'https://aai-logon.vho-switchaai.ch/idp/profile/Shibboleth/SSO',
+$IDProviders['https://vho.example.org/idp/shibboleth'] = array (
+        'Type' => 'vho',
+        'Name' => 'Virtual Home Organisation',
+        'en' => array (
+            'Name' => 'Virtual Home Organisation',
+            'Keywords','Zurich Switzerland',
+            ),
+        'de' => array (
+            'Name' => 'Virtuelle Home Organisation',
+            'Keywords','Zürich Schweiz',
+            ),
+        'fr' => array ('Name' => 'Home Organisation Virtuelle'),
+        'it' => array ('Name' => 'Virtuale Home Organisation'),
+        'IP' => array ('130.59.6.0/16','127.0.0.0/24'),
+        'SSO' => 'https://vho.example.org/idp/profile/Shibboleth/SSO',
 );
 
 // Example of an IDP you want not to be displayed when IDPs are parsed from
 // a metadata file and SAML2MetaOverLocalConf is set to false
-//$IDProviders['urn:mace:switch.ch:SWITCHaai:invisibleidp'] = '-';
+//$IDProviders['https://test-uni3.example.org/idp/shibboleth'] = '-';
 
 
 // Category
 $IDProviders['unknown'] = array (
-		'Type' => 'category',
-		'Name' => 'Others',
-		'de' => array ('Name' => 'Andere'),
-		'fr' => array ('Name' => 'Autres'),
-		'it' => array ('Name' => 'Altri'),
+        'Type' => 'category',
+        'Name' => 'Others',
+        'de' => array ('Name' => 'Andere'),
+        'fr' => array ('Name' => 'Autres'),
+        'it' => array ('Name' => 'Altri'),
 );
 
 ?>
